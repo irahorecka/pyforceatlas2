da1ad1adb463cfa4065baa843a516a211359fc3c
Amin Alam <ma.alamalhoda@gmail.com>
2024-05-07 19:16:50 +0200

    new version

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ print(">>>> Starting to install!\n")
 
 setup(
     name='fa2_modified',
-    version='0.3.9',
+    version='0.3.10',
     description='The fastest ForceAtlas2 algorithm for Python (and NetworkX)',
     long_description_content_type='text/markdown',
     long_description=long_description,
