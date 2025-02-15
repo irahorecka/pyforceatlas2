22853c33834c61ba01cdbc802ee14bb10639f9bf
Ira Horecka <ira89@icloud.com>
2025-02-15 16:09:18 -0500

    update pypi config files

--- setup.py
+++ setup.py
@@ -3,21 +3,26 @@ setup
 ~~~~~
 """
 
-import os
+import re
 from setuptools import setup, find_packages
 
 
-# Read the contents of your README file
-with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as fh:
-    long_description = fh.read()
+def find_version():
+    """Function to find the version string in the __init__.py file."""
+    with open("risk/__init__.py", "r", encoding="utf-8") as f:
+        version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read(), re.M)
+        if version_match:
+            return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name="pyforceatlas2",
-    version="0.0.1",
+    version=find_version(),  # Dynamically fetches the version from the __init__.py file.
     author="Ira Horecka",
     author_email="ira89@icloud.com",
     description="Fast Gephi's ForceAtlas2 graph layout algorithm written in Python for Python.",
-    long_description=long_description,
+    long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/irahorecka/pyforceatlas2",
     packages=find_packages(),
