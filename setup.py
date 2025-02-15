9ec240a2a2db8d67a1196e597d606425ecfc93f3
Ira Horecka <ira89@icloud.com>
2025-02-15 09:49:47 -0500

    update setup and toml files

--- setup.py
+++ setup.py
@@ -1,73 +1,44 @@
-from codecs import open
-from os import path
+"""
+setup
+~~~~~
+"""
 
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
 
-print("Installing fa2_modified package (fastest forceatlas2 python implementation)\n")
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), 'r') as f:
-    long_description = f.read()
-
-print(">>>> Cython is installed?")
-try:
-    from Cython.Distutils import Extension
-    from Cython.Build import build_ext, cythonize
-    USE_CYTHON = True
-    print('Yes\n')
-except ImportError:
-    from setuptools.extension import Extension
-    USE_CYTHON = False
-    print('Cython is not installed; using pre-generated C files if available')
-    print('Please install Cython first and try again if you face any installation problems\n')
-    print(">>>> Are pre-generated C files available?")
-
-if USE_CYTHON:
-    ext_modules = cythonize([Extension('fa2_modified.fa2util', ['fa2_modified/fa2util.py'], cython_directives={'language_level' : 3})])
-    cmdclass = {'build_ext': build_ext}
-    opts = {"ext_modules": ext_modules, "cmdclass": cmdclass}
-elif path.isfile(path.join(here, 'fa2_modified/fa2util.c')):
-    print("Yes\n")
-    ext_modules = [Extension('fa2_modified.fa2util', ['fa2_modified/fa2util.c'])]
-    cmdclass = {}
-    opts = {"ext_modules": ext_modules, "cmdclass": cmdclass}
-else:
-    print("Pre-generated C files are not available. This library will be slow without Cython optimizations.\n")
-    opts = {"py_modules": ["fa2_modified.fa2util"]}
-
-# Uncomment the following line if you want to install without optimizations
-# opts = {"py_modules": ["fa2.fa2util"]}
-
-print(">>>> Starting to install!\n")
+# Read the contents of your README file
+with open(os.path.join(os.path.dirname(__file__), "README.md"), encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name='fa2_modified',
-    version='0.3.10',
-    description='The fastest ForceAtlas2 algorithm for Python (and NetworkX)',
-    long_description_content_type='text/markdown',
+    name="pyforceatlas2",
+    version="0.0.1",
+    author="Ira Horecka",
+    author_email="ira89@icloud.com",
+    description="Fast Gephi's ForceAtlas2 graph layout algorithm written in Python for Python.",
     long_description=long_description,
-    author='Bhargav Chippada, Amin Alam',
-    url='https://github.com/AminAlam/forceatlas2_maintained',
-    keywords=['forceatlas2', 'networkx', 'force-directed-graph', 'force-layout', 'graph'],
-    packages=['fa2_modified'],
+    long_description_content_type="text/markdown",
+    url="https://github.com/irahorecka/pyforceatlas2",
+    packages=find_packages(),
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Mathematics',
-
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3'
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    install_requires=['numpy', 'scipy', 'tqdm'],
-    extras_require={
-        'networkx': ['networkx'],
-        'igraph': ['python-igraph']
-    },
-    include_package_data=True,
-    **opts
+    python_requires=">=3.8",
+    install_requires=[
+        "networkx",
+        "numpy",
+        "python-igraph",
+        "scipy",
+        "tqdm",
+    ],
 )
